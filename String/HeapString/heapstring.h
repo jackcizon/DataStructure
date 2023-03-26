eaee6dc1d0c93273ebdad90ded98717e60ab1f5e
jack_cizon <105433087+infinityjack@users.noreply.github.com>
2023-03-26 16:25:50 +0800

    Update heapstring.h

--- String/HeapString/heapstring.h
+++ String/HeapString/heapstring.h
@@ -1,6 +1,5 @@
 #ifndef HEAPSTRING_H
 #define HEAPSTRING_H
-#endif
 
 #include<stdio.h>
 #include<stdbool.h>
@@ -44,3 +43,5 @@ void Set(HeapStr* HS, size_t index, char ch);
 void Repalce(HeapStr* HS, size_t begin, string str);
 
 void Echo(HeapStr* HS);
+
+#endif
