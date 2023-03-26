aec25ab8349bc7b2aa1951dcd9030287d666f878
jack_cizon <105433087+infinityjack@users.noreply.github.com>
2023-03-26 16:21:42 +0800

    Update expression.h

--- Stack/examples/ExpressionEvaluation/expression.h
+++ Stack/examples/ExpressionEvaluation/expression.h
@@ -1,3 +1,6 @@
+#ifndef EXPRESSION_H
+#define EXPERSSION_H
+
 void InToPost(const char* infix, char* postfix);
 
 int precedence(char symbol);
@@ -5,3 +8,5 @@ int precedence(char symbol);
 void PostEval(const char* postfix);
 
 void PrintExpression(const char* postfix);
+
+#endif
