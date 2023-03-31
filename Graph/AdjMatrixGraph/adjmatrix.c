273320710e3540b554520432d5cf209fca5cbce8
jack_cizon <105433087+jackcizon@users.noreply.github.com>
2023-03-31 18:40:44 +0800

    Update adjmatrix.c

--- Graph/AdjMatrixGraph/adjmatrix.c
+++ Graph/AdjMatrixGraph/adjmatrix.c
@@ -1,14 +1,15 @@
 #include"adjmatrix.h"
 
-Graph* NewGraph(size_t size, GraphType type)
+Graph* NewGraph(size_t Vertices, GraphType type)
 {
     Graph* G = malloc(sizeof *G);
-    G->size = size;
+    G->Vertices = Vertices;
+    G->Arcs = 0;
     G->type = type;
-    G->matrix = malloc(size * sizeof(int*));
-    for (size_t i = 0; i < size; i++)
+    G->matrix = malloc(Vertices * sizeof(int*));
+    for (size_t i = 0; i < Vertices; i++)
     {
-        G->matrix[i] = calloc(size, sizeof(int));
+        G->matrix[i] = calloc(Vertices, sizeof(int));
     }
     return G;
 }
@@ -21,6 +22,7 @@ void AddEdge(Graph* G, size_t src, size_t dest, size_t weight)
     {
         G->matrix[dest][src] = weight;
     }
+    G->Arcs += 1;
 }
 
 
@@ -31,22 +33,23 @@ void DeleteEdge(Graph* G, size_t src, size_t dest)
     {
         G->matrix[dest][src] = 0;
     }
+    G->Arcs -= 1;
 }
 
 
 void Traverse(Graph* G)
 {
     printf("    ");
-    for (size_t i = 0; i < G->size; i++)
+    for (size_t i = 0; i < G->Vertices; i++)
     {
         printf("%4d", i);
     }
     printf("\n\n");
     
-    for (size_t i = 0; i < G->size; i++)
+    for (size_t i = 0; i < G->Vertices; i++)
     {
         printf("%2ld: ", i);
-        for (size_t j = 0; j < G->size; j++)
+        for (size_t j = 0; j < G->Vertices; j++)
         {
             printf("%4d", G->matrix[i][j]);
         }
@@ -57,7 +60,7 @@ void Traverse(Graph* G)
 
 void Destroy(Graph* G)
 {
-    for (size_t i = 0; i < G->size; i++)
+    for (size_t i = 0; i < G->Vertices; i++)
     {
         free(G->matrix[i]);
     }
@@ -69,9 +72,9 @@ void Destroy(Graph* G)
 
 int* GetAdjVertices(Graph* G, int vertex, int* count)
 {
-    int* adjacent = malloc(G->size * sizeof(int));
+    int* adjacent = malloc(G->Vertices * sizeof(int));
     *count = 0;
-    for (size_t i = 0; i < G->size; i++)
+    for (size_t i = 0; i < G->Vertices; i++)
     {
         //check columns
         if (G->matrix[vertex][i] != 0)
@@ -87,4 +90,16 @@ int* GetAdjVertices(Graph* G, int vertex, int* count)
     }
     adjacent = realloc(adjacent, *count * sizeof(int));
     return adjacent;
+}
+
+
+size_t NumVertices(Graph* G)
+{
+    return G->Vertices;
+}
+
+
+size_t NumArcs(Graph* G)
+{
+    return G->Arcs;
 }
