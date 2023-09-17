3292ca061ccd4c2942a8f3a37a9e0da62f1c0046
jackcizon <jack20021213cn@gmail.com>
2023-09-17 21:46:38 +0800

    commit sparse files

---
+++ Matrix/SparseMatrix/sparse_main.c
@@ -0,0 +1,8 @@
+#include"sparse_matrix.h"
+
+function int main(int argc, char const *argv[])
+{
+    a();
+
+    return 0;
+}
---
+++ Matrix/SparseMatrix/sparse_matrix.h
@@ -0,0 +1,8 @@
+#ifndef SPARSE_MATRIX_H
+#define SPARSE_MATRIX_H
+
+#include"config.h"
+
+void a();
+
+#endif
