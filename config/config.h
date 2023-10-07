54a4cb1c704c1e63fd3fae9efc94c382aae1fd95
jackcizon <jack20021213cn@gmail.com>
2023-10-08 00:09:06 +0800

    update

---
+++ config/config.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define eq ==
+#define ineq !=
+#define gt >
+#define lt <
+#define gte >=
+#define lte <=
+#define and &&
+#define or ||
+#define not !
+#define pass 0
+#define NIL (void*) 0
+#define begin {
+#define end }
+#define function
+#define PUBLIC
+#define PRIVATE static
+#define true 1
+#define false 0
+
+typedef char* string;
